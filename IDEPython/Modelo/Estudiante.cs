5abc028d4478f8d0aa36e35fae6761099fca0537
XandersEG <166416633+XandersEG@users.noreply.github.com>
2026-05-13 02:46:27 -0600

    Cambios en Estudiante

--- IDEPython/Modelo/Estudiante.cs
+++ IDEPython/Modelo/Estudiante.cs
@@ -8,5 +8,14 @@ namespace IDEPython.Modelo
     public class Estudiante : Usuario
     {
         public List<Curso> Cursos { get; set; } = new List<Curso>();
+
+        public Estudiante(String fn, String sn, String fl, String sl, String mail)
+        {
+            this.FirstName = fn;
+            SecondName = sn;
+            LastName1 = fl;
+            LastName2 = sl;
+            Email = mail;
+        }
     }
 }
