525a3a7dd10fb903e823630c467c3e15d4079fa7
XandersEG <166416633+XandersEG@users.noreply.github.com>
2026-05-18 17:47:54 -0600

    Preparing for the PHP connection

---
+++ IDEPython/ApiService.cs
@@ -0,0 +1,63 @@
+﻿using System.Net.Http;
+using System.Text;
+using System.Text.Json;
+
+namespace IDEPython
+{
+    public class ApiService
+    {
+        private readonly HttpClient client;
+
+        public string Token { get; set; }
+
+        public ApiService()
+        {
+            client = new HttpClient();
+
+            client.BaseAddress =
+                new Uri("http://138.2.235.169/ide/web/controlador/");
+        }
+
+        public async Task<string> GetAsync(string endpoint)
+        {
+            if (!string.IsNullOrEmpty(Token))
+            {
+                client.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue(
+                        "Bearer",
+                        Token
+                    );
+            }
+
+            HttpResponseMessage response =
+                await client.GetAsync(endpoint);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+
+        public async Task<string> PostAsync(string endpoint, object datos)
+        {
+            if (!string.IsNullOrEmpty(Token))
+            {
+                client.DefaultRequestHeaders.Authorization =
+                    new System.Net.Http.Headers.AuthenticationHeaderValue(
+                        "Bearer",
+                        Token
+                    );
+            }
+
+            string json = JsonSerializer.Serialize(datos);
+
+            var content = new StringContent(
+                json,
+                Encoding.UTF8,
+                "application/json"
+            );
+
+            HttpResponseMessage response =
+                await client.PostAsync(endpoint, content);
+
+            return await response.Content.ReadAsStringAsync();
+        }
+    }
+}
---
+++ IDEPython/Logica/LoginResponse.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace IDEPython.Logica
+{
+    public class LoginResponse
+    {
+        public bool exito { get; set; }
+
+        public string token { get; set; }
+
+        public Usuario usuario { get; set; }
+    }
+
+    public class Usuario
+    {
+        public string correo { get; set; }
+
+        public string nombre { get; set; }
+
+        public string rol { get; set; }
+    }
+}
