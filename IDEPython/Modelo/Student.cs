018b281041cfa0db6c56362fb2e650c28c4f51d8
XandersEG <166416633+XandersEG@users.noreply.github.com>
2026-05-18 22:59:36 -0600

    Model.Student

--- IDEPython/Modelo/Student.cs
+++ IDEPython/Modelo/Student.cs
@@ -7,15 +7,15 @@ namespace IDEPython.Modelo
 {
     public class Student : User
     {
-        public List<Course> Courses { get; set; } = new List<Course>();
+        public List<Course> Courses { get; set; } = new();
 
-        public Student(String fn, String sn, String fl, String sl, String mail)
+        public Student(String firstName, String secondName, String lastName1, String lastName2, String email)
         {
-            this.FirstName = fn;
-            SecondName = sn;
-            LastName1 = fl;
-            LastName2 = sl;
-            Email = mail;
+            this.FirstName = firstName;
+            SecondName = secondName;
+            LastName1 = lastName1;
+            LastName2 = lastName2;
+            Email = email;
         }
     }
 }
