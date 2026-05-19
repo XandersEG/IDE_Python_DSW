7a5e9a61d9b02c3e7f6d2284c70542e48113b935
XandersEG <166416633+XandersEG@users.noreply.github.com>
2026-05-19 14:58:55 -0600

    New attributes now included

--- IDEPython/Logica/RegisterRequest.cs
+++ IDEPython/Logica/RegisterRequest.cs
@@ -7,9 +7,9 @@ namespace IDEPython.Logica
     public class RegisterRequest
     {
         public string primerNombre { get; set; }
-        //public string segundoNombre { get; set; }
+        public string segundoNombre { get; set; }
         public string primerApellido { get; set; }
-        //public string segundoApellido { get; set; }
+        public string segundoApellido { get; set; }
         public string correo { get; set; }
         public string contrasena { get; set; }
         public string confirmarContrasena { get; set; }
