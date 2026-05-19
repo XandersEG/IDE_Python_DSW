0900aaf604953ca6fc33d632fc0161fbe020db2e
XandersEG <166416633+XandersEG@users.noreply.github.com>
2026-05-19 13:06:34 -0600

    PHP logic for Login

--- IDEPython/VistaLogin.xaml.cs
+++ IDEPython/VistaLogin.xaml.cs
@@ -1,7 +1,9 @@
-﻿using IDEPython.Modelo;
+﻿using IDEPython.Logica;
+using IDEPython.Modelo;
 using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Text.Json;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -26,23 +28,64 @@ namespace IDEPython
         
         private ApiService api = new ApiService();
 
-
-        private void btnIniciarSesion_Click(object sender, RoutedEventArgs e)
+        private async void btnIniciarSesion_Click(object sender, RoutedEventArgs e)
         {
+
             var datos = new
             {
                 correo = txtEmail.Text,
                 contrasena = txtPassword.Password
             };
-            // Still missing credentials validation logic
-            // TODO: Get user info from BackEnd
 
-            Student user = new Student("Xanders", "Makenssy", "Espinoza", "Guzman", "x.espinoza.1@estudiantec.cr");
+            string respuesta = await api.PostAsync(
+                "register",
+                datos
+            );
+
+            MessageBox.Show(respuesta);
+
+            try
+            {
+                LoginResponse? login =
+                    JsonSerializer.Deserialize<LoginResponse>(respuesta);
+
+                if (login == null)
+                {
+                    MessageBox.Show("Respuesta inválida");
+                    return;
+                }
+
+                else { MessageBox.Show("no nulo"); }
+
+                if (login.exito)
+                {
+                    api.Token = login.token;
+
+                    Student user = new Student(
+                        login.usuario.nombre,
+                        "",
+                        "",
+                        "",
+                        login.usuario.correo
+                    );
 
                     //Student user = new Student("Xanders", "Makenssy", "Espinoza", "Guzman", datos.correo);
                     VistaCursos window = new VistaCursos(user, api);
                     window.Show();
             this.Close();
+        }
+                else
+                {
+                    MessageBox.Show("Credenciales inválidas");
+                }
+
+
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
         }
 
         private void irARegistro_Click(object sender, MouseButtonEventArgs e)
