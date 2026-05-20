d7955ae65e51ca4707e9526aec3cb30e885828ff
XandersEG <166416633+XandersEG@users.noreply.github.com>
2026-05-20 16:02:59 -0600

    Enhanced user experience

--- IDEPython/VistaLogin.xaml.cs
+++ IDEPython/VistaLogin.xaml.cs
@@ -23,13 +23,42 @@ namespace IDEPython
         public VistaLogin()
         {
             InitializeComponent();
+            txtEmail.Focus();
 
         }
         
+        public VistaLogin(String correo)
+        {
+            InitializeComponent();
+            txtEmail.Text = correo;
+            txtPassword.Focus();
+
+        }
+
         private ApiService api = new ApiService();
 
         private async void btnIniciarSesion_Click(object sender, RoutedEventArgs e)
         {
+
+            Student us = new Student("Xanders", "Makenssy", "Espinoza", "Guzman", "x.espinoza.1@estudiantec.cr");
+            VistaCursos win = new VistaCursos(us, api);
+            win.Show();
+            this.Close();
+            return;
+
+            if (string.IsNullOrWhiteSpace(txtEmail.Text))
+            {
+                MessageBox.Show("Debe de ingresar un correo para iniciar sesión.", "Validacion", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtEmail.Focus();
+                return;
+            }
+
+            if (string.IsNullOrWhiteSpace(txtPassword.Password))
+            {
+                MessageBox.Show("Debe de ingresar una contraseña para iniciar sesión.", "Validacion", MessageBoxButton.OK, MessageBoxImage.Warning);
+                txtPassword.Focus(); 
+                return;
+            }
 
             var datos = new
             {
@@ -42,8 +71,6 @@ namespace IDEPython
                 datos
             );
 
-            MessageBox.Show(respuesta);
-
             try
             {
                 LoginResponse? login =
@@ -51,42 +78,35 @@ namespace IDEPython
 
                 if (login == null)
                 {
-                    MessageBox.Show("Respuesta inválida");
+                    MessageBox.Show("Respuesta inválida de parte del servidor");
                     return;
                 }
-
-                else { MessageBox.Show("no nulo"); }
 
                 if (login.exito)
                 {
                     api.Token = login.token;
 
                     Student user = new Student(
-                        login.usuario.nombre,
+                        login.datos.nombre+login.datos.apellido,
                         "",
                         "",
                         "",
-                        login.usuario.correo
+                        login.datos.correo
                     );
 
-                    //Student user = new Student("Xanders", "Makenssy", "Espinoza", "Guzman", datos.correo);
                     VistaCursos window = new VistaCursos(user, api);
                     window.Show();
             this.Close();
         }
                 else
                 {
-                    MessageBox.Show("Credenciales inválidas");
+                    MessageBox.Show("Las credenciales que ha ingresado no son correctas. Verifique que el correo y contraseña ingresados sean correctos","Credenciales inválidas", MessageBoxButton.OK, MessageBoxImage.Error);
                 }
 
             }
             catch (Exception ex)
             {
                 MessageBox.Show(ex.Message);
-                Student user = new Student("Usuario no cargado", "", "", "", "");
-                VistaCursos window = new VistaCursos(user, api);
-                window.Show();
-                this.Close();
             }
             
         }
